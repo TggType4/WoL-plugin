ad3b4f85420b933baa020ee52cf6c8f726ade868
TggType4 <tggtype4@gmail.com>
2023-08-21 22:15:16 +0200

    sql fix and error logging

--- wol-plugin.php
+++ wol-plugin.php
@@ -4,11 +4,27 @@
  * 
  * Plugin Name: Wol Plugin
  * Description: A plugin that lets you see statuses of desktops and turn them on, use shortcode [statuses] to display the menu
- * Version: 0.2.3
+ * Version: 0.2.4
  * Text Domain: options-plugin
  * 
  * 
  */
+
+
+if (!function_exists('write_log')){
+    function write_log($log){
+        if (true === WP_DEBUG){
+            if (is_array($log) || is_object($log)){
+                error_log(print_r($log, true));
+            }
+            else {
+                error_log($log);
+            }
+        }
+    }
+}
+
+
 
 
 register_activation_hook(__FILE__, 'create_desktops_table');
@@ -25,12 +41,15 @@ function create_desktops_table(){
         $sql = "CREATE TABLE $table_name (
             id INT NOT NULL AUTO_INCREMENT,
             name VARCHAR(64) NOT NULL,
-            ip VARCHAR(12) NOT NULL,
+            ip VARCHAR(15) NOT NULL,
             mac VARCHAR(18) NOT NULL,
             PRIMARY KEY (id)
         ) $charset_collate;";
         require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
         dbDelta($sql);
+    }
+    if ($wpdb->last_error !== ""){
+        write_log($wpdb->last_error);
     }
 }
 
@@ -110,78 +129,67 @@ function add_desktop($data){
     wp_set_current_user($current_user_id);
     $nonce = $params["admin_nonce"];
     $action = $params["action"];
-    if (wp_verify_nonce($nonce, "admin_nonce")){
-        $check_if_in_query = $wpdb->prepare("SELECT name FROM $table_name WHERE name = %s", $params["name"]);
-        $check_if_in_result = $wpdb->get_results($check_if_in_query);
-        if ($action == "add"){
-            if (!$check_if_in_result){
-                $desktop = array(
-                    "name" => $params["name"],
-                    "ip" => $params["ip"],
-                    "mac" => $params["mac"]
-                );
-                $result = $wpdb->insert($table_name, $desktop);
-                if ($result){
-                    echo "success_add";
-                }
-                else {
-                    echo "error_add";
-                }
-            }
-            else {
-                echo "error_already_exists";
-            }
-
-            /*
-            if (!isset($desktops[$params["name"]])){
-                $desktops[$params["name"]] = array(
-                    "ip" => $params["ip"],
-                    "mac" => $params["mac"]
-                );
-                echo "success_add";
-            }
-            else {
-                echo "error_already_exists";
-            }*/
-        }
-        elseif ($action == "update"){
-            if ($check_if_in_result){
-                $desktop_data = array(
-                    "ip" => $params["ip"],
-                    "mac" => $params["mac"]
-                );
-                $condition = array(
-                    "name" => $params["name"]
-                );
-                $result = $wpdb->update($table_name, $desktop_data, $condition);
-                if ($result){
-                    echo "success_update";
-                }
-                else {
-                    echo "error_update";
-                }
-            }   
-            else {
-                echo "error_non_existent";
-            }
-            /*
-            if (isset($desktops[$params["name"]])){
-                $desktops[$params["name"]] = array(
-                    "ip" => $params["ip"],
-                    "mac" => $params["mac"]
-                );
-                echo "success_update";
-            }
-            else {
-                echo "error_non_existent";
-            }*/
-        }
-        else {
-            echo "action_error";
-        }
+    $regex_ip = '/\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/';
+    $regex_mac = '/\b([0-9A-Fa-f]{1,2}:){5}[0-9A-Fa-f]{1,2}\b/';
+    if (preg_match($regex_ip, $params["ip"]) !== 1){
+        echo "Invalid ip address format: ". $params["ip"];
+    }
+    elseif (preg_match($regex_mac, $params["mac"]) !== 1){
+        echo "Invalid mac address format: ". $params["mac"];
     }
     else {
-        echo "nonce_error";
+        if (wp_verify_nonce($nonce, "admin_nonce")){
+            $check_if_in_query = $wpdb->prepare("SELECT name FROM $table_name WHERE name = %s", $params["name"]);
+            $check_if_in_result = $wpdb->get_results($check_if_in_query);
+            if ($action == "add"){
+                if (!$check_if_in_result){
+                    $desktop = array(
+                        "name" => $params["name"],
+                        "ip" => $params["ip"],
+                        "mac" => $params["mac"]
+                    );
+                    $result = $wpdb->insert($table_name, $desktop);
+                    if ($result){
+                        echo "success_add";
+                    }
+                    else {
+                        write_log($wpdb->last_error);
+                        echo $wpdb->last_error;
+                    }
+                }
+                else {
+                    echo "error_already_exists";
+                }
+            }
+            elseif ($action == "update"){
+                if ($check_if_in_result){
+                    $desktop_data = array(
+                        "ip" => $params["ip"],
+                        "mac" => $params["mac"]
+                    );
+                    $condition = array(
+                        "name" => $params["name"]
+                    );
+                    $result = $wpdb->update($table_name, $desktop_data, $condition);
+                    if ($result){
+                        echo "success_update";
+                    }
+                    else {
+                        write_log($wpdb->last_error);
+                        echo $wpdb->last_error;
+                    }
+                }   
+                else {
+                    echo "error_non_existent";
+                }
+            }
+            else {
+                echo "action_error";
+            }
+        }
+        else {
+            echo "nonce_error";
+        }
     }
 }
 
@@ -207,22 +215,13 @@ function del_desktop($data){
                 echo "success_del";
             }
             else {
-                echo "error_del";
+                write_log($wpdb->last_error);
+                echo $wpdb->last_error;
             }
         }
         else {
             echo "error_non_existent";
         }
-        /*
-        if (isset($desktops[$params["name"]])){
-            unset($desktops[$params["name"]]);
-            $desktops_jsonstr = json_encode($desktops);
-            file_put_contents(plugin_dir_path( __FILE__ )."desktops.json", $desktops_jsonstr);
-            echo "success_del";
-        }
-        else {
-            echo "error_non_existent";
-        }*/
     }
     else {
         echo "nonce_error";
@@ -310,13 +309,16 @@ function get_status($data){
     socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, array('sec' => 1.5, 'usec' => 0));
     socket_connect($socket, $ip, 0);
     socket_send($socket, $icmp_packet, strlen($icmp_packet), 0);
-    if (socket_read($socket, 255)){
+    $result = socket_read($socket, 255);
+    write_log("ping_reply: code: ". socket_last_error($socket). " content: ". bin2hex($result));
+    if ($result){
         echo "online";
     }
     else {
         echo "offline";
     }
     socket_close($socket);
+
 }
 
 function get_status_old($data){
@@ -357,6 +359,7 @@ function send_wol($data){
         $socket = socket_create(AF_INET, SOCK_DGRAM, getprotobyname("udp"));
         socket_set_option($socket, SOL_SOCKET, SO_BROADCAST, 1);
         $magic_packet_sent = socket_sendto($socket, $magic_packet, strlen($magic_packet), 0, "255.255.255.255", 9);
+        write_log("wol_reply: code: ". socket_last_error($socket). " content: ". bin2hex($magic_packet_sent));
         socket_close($socket);
         if ($magic_packet_sent){
             echo "sent";
